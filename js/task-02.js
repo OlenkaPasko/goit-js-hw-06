71c1d5acd92e075d7575d59e2e52c836fc3c0abd
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-02-22 00:09:04 +0200

    task-02(1)

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+//Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//Додасть назву інгредієнта як його текстовий вміст.
+//Додасть елементу клас item.
+//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+//const heading = document.createElement("h1");
+//console.log(heading); // <h1></h1>
+
+//heading.textContent = "This is a heading";
+//console.log(heading); // <h1>This is a heading</h1>
+const ingredientsList = document.querySelector('ingredients');
+const ingredientsItems = document.createElement("li");
+item.textContent = "ingredients";
+list.append(ingredientsItems);
+console.log(ingredientsList);
+
+// `<li>${item}</li>`, '');
