6704120ff45feca024625ee4906e6bc29027d98d
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-02-22 09:10:49 +0200

    task-02(2)

--- js/task-02.js
+++ js/task-02.js
@@ -1,24 +1,36 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
 //Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
 //Додасть назву інгредієнта як його текстовий вміст.
 //Додасть елементу клас item.
 //Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
-//const heading = document.createElement("h1");
-//console.log(heading); // <h1></h1>
-
-//heading.textContent = "This is a heading";
-//console.log(heading); // <h1>This is a heading</h1>
-const ingredientsList = document.querySelector('ingredients');
-const ingredientsItems = document.createElement("li");
-item.textContent = "ingredients";
-list.append(ingredientsItems);
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsList = document.querySelector(".ingredients");
+const ingredientsItems = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
 console.log(ingredientsList);
 
-// `<li>${item}</li>`, '');
+//const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+//const list = document.querySelector(".list");
+
+//const markup = technologies
+  //.map((technology) => `<li class="list-item">${technology}</li>`)
+//.join("");
+
+// Check the console, you'll see a single string with HTML tags
+//.log(markup);
+
+// Adding all the markup in one operation
+//list.innerHTML = markup;
+
+
+
