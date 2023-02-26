345f16dbcba4389ffc1d1bd0b7c96cf6343d31f7
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-02-26 14:00:31 +0200

    task - 04(6)

--- js/task-04.js
+++ js/task-04.js
@@ -13,17 +13,17 @@
 let counterValue = 0;
 const increment = document.querySelector(`[data-action = "increment"]`);
 const decrement = document.querySelector(`[data-action = "decrement"]`);
-const valueRef = document.getElementById("value"); 
+const valueRef = document.getElementById("value");
 
 const clickDecrement = (event) => {
   counterValue -= 1;
   valueRef.textContent = counterValue;
-} 
+};
 
-  const clickIncrement = (event) => {
-    counterValue += 1;
-    valueRef.textContent = counterValue;
-  }
+const clickIncrement = (event) => {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+};
 decrement.addEventListener("click", clickDecrement);
 increment.addEventListener("click", clickIncrement);
 
