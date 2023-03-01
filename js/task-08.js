ef309b4ea0ed5ab9fb0604958487e6ee487698ee
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-01 16:08:15 +0200

    доопрацювання

--- js/task-08.js
+++ js/task-08.js
@@ -3,19 +3,16 @@ form.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
   event.preventDefault();
-  const formEl = event.currentTarget.elements;
-  const emailEl = formEl.email.value;
-  const passwordEl = formEl.password.value;
 
-  const formAllattribute = {
-    emailEl,
-    passwordEl,
-  };
-  if (emailEl === " " || passwordEl === " ") {
-    alert("Please fill in all the fields!");
+  const { email, password } = event.currentTarget.elements;
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert("Please fill in all the fields!");
   } else {
-    console.log(formAllattribute);
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
   }
-
-  event.currentTarget.reset();
 }
